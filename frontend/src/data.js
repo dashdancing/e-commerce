1f3f24ce10362c024e59a869be1f44e8f3e670dc
Omar <omar.gbet@gmail.com>
2021-02-14 23:01:10 -0600

    add folder Component. Create Product and Rating component

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -7,7 +7,7 @@ const data = {
             image: '/assets/product-1.jpg',
             price: 350,
             brand: 'Supreme',
-            raiting: 4.5,
+            rating: 4.5,
             numReviews: 10,
             description: 'Colaboración con la marca de lujo Gucci'
         },
@@ -18,7 +18,7 @@ const data = {
             image: '/assets/product-2.jpg',
             price: 300,
             brand: 'Supreme',
-            raiting: 4.5,
+            rating: 4.5,
             numReviews: 15,
             description: 'Colaboración con la marca de lujo Gucci'
         },
@@ -29,7 +29,7 @@ const data = {
             image: '/assets/product-3.jpg',
             price: 150,
             brand: 'Supreme',
-            raiting: 4.5,
+            rating: 4.5,
             numReviews: 18,
             description: 'Colaboración con la marca de lujo Gucci'
         },
@@ -40,7 +40,7 @@ const data = {
             image: '/assets/product-2.jpg',
             price: 250,
             brand: 'Supreme',
-            raiting: 4.5,
+            rating: 4.5,
             numReviews: 19,
             description: 'Colaboración con la marca de lujo Gucci'
         },
@@ -51,7 +51,7 @@ const data = {
             image: '/assets/product-3.jpg',
             price: 200,
             brand: 'Supreme',
-            raiting: 4.5,
+            rating: 4.5,
             numReviews: 13,
             description: 'Colaboración con la marca de lujo Gucci'
         },
