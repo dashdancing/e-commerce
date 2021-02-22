c9201da1e72116325da6c31c0aa960691f72aa1e
Omar <omar.gbet@gmail.com>
2021-02-22 00:51:23 -0600

    ADD Redux, create folder constants &  actions

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import {compose, createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import data from './data';
+
+const initialState = {};
+const reducer = (state, action) => {
+    return { products: data.products};
+};
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer,
+    initialState,
+    composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;
