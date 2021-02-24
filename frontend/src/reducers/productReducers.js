920b4e391825a2a66e0a23f96ec96e698055ff25
Omar <omar.gbet@gmail.com>
2021-02-23 22:13:16 -0600

    ADD Redux Product Screen Route ID

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,8 +2,10 @@ const {
     PRODUCT_LIST_REQUEST,
     PRODUCT_LIST_SUCCESS,
     PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST,
+    PRODUCT_DETAILS_SUCCESS,
+    PRODUCT_DETAILS_FAIL,
 } = require ('../constants/productConstants');
-
 
 export const productListReducer = (
     state = {loading: true, products: []}, 
@@ -19,3 +21,18 @@ export const productListReducer = (
         return state;
     }
 }
+
+export const productDetailsReducer = (
+    state = { product: {}, loading: true}, 
+    action) => {
+        switch (action.type) {
+            case PRODUCT_DETAILS_REQUEST:
+                return {loading: true};
+            case PRODUCT_DETAILS_SUCCESS:
+                return {loading: false, product: action.payload};
+            case PRODUCT_DETAILS_FAIL:
+                return {loading: false, error: action.payload};
+            default: 
+                return state;
+        }
+    };
